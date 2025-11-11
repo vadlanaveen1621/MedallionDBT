ccd7372f5f47011d99e530797b8acef431fef49b
Naveen Krishnappa <vadlanaveen1621@gmail.com>
2025-11-11 20:20:59 +0100

    Update stg_customers_json.sql

--- models/staging/stg_customers_json.sql
+++ models/staging/stg_customers_json.sql
@@ -1,9 +1,10 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- else -%}
-        {{ custom_schema_name | upper }}
-    {%- endif -%}
-{%- endmacro %}
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+SELECT 
+    src AS raw_json_data,
+    CURRENT_TIMESTAMP() AS loaded_at
+FROM {{ source('raw', 'customers_json') }}
