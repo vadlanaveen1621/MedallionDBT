669d16aab509528a638adc53e9d427451f81e533
Naveen Krishnappa <vadlanaveen1621@gmail.com>
2025-11-11 20:17:56 +0100

    Update raw_orders_csv.sql

--- models/bronze/raw_orders_csv.sql
+++ models/bronze/raw_orders_csv.sql
@@ -1,9 +1,18 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- else -%}
-        {{ custom_schema_name | upper }}
-    {%- endif -%}
-{%- endmacro %}
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id'
+    )
+}}
+
+SELECT 
+    order_id::VARCHAR AS order_id,
+    customer_id::VARCHAR AS customer_id,
+    TRY_TO_DATE(order_date) AS order_date,
+    amount::DECIMAL(10,2) AS amount,
+    status::VARCHAR AS status,
+    loaded_at
+FROM {{ ref('stg_orders_csv') }}
+{% if is_incremental() %}
+WHERE loaded_at > (SELECT MAX(loaded_at) FROM {{ this }})
+{% endif %}
