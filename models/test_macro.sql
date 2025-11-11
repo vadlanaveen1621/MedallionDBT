04b28b8fa449add451542c43c551d72750699c5d
Naveen Krishnappa <vadlanaveen1621@gmail.com>
2025-11-11 20:21:57 +0100

    Update test_macro.sql

--- models/test_macro.sql
+++ models/test_macro.sql
@@ -1,9 +1,11 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- else -%}
-        {{ custom_schema_name | upper }}
-    {%- endif -%}
-{%- endmacro %}
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    {{ generate_surrogate_key('1') }} as test_key_1,
+    {{ generate_surrogate_key("'hello'") }} as test_key_2,
+    {{ generate_surrogate_key(["1", "'world'"]) }} as test_key_3,
+    {{ generate_surrogate_key(["'test'", "'composite'", "'key'"]) }} as test_key_4
