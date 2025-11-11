612230e3d48a7c0d8fb4706c6038f619668effa0
Naveen Krishnappa <vadlanaveen1621@gmail.com>
2025-11-11 20:21:14 +0100

    Update stg_orders_csv.sql

--- models/staging/stg_orders_csv.sql
+++ models/staging/stg_orders_csv.sql
@@ -1,9 +1,14 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- else -%}
-        {{ custom_schema_name | upper }}
-    {%- endif -%}
-{%- endmacro %}
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+SELECT 
+    order_id,
+    customer_id,
+    order_date,
+    amount,
+    status,
+    CURRENT_TIMESTAMP() AS loaded_at
+FROM {{ source('raw', 'orders_csv') }}
